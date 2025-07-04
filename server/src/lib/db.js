436d2b77701a4b383ba797434e0454a8e03a9ae7
Kushagra-Dobriyal <kushagra.dobriyal@gmail.com>
2025-07-04 17:59:06 +0530

    updated ci/cd

--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -4,7 +4,7 @@ export const connectDB=async()=>{
     try {
         const conn=await mongoose.connect(process.env.MONGO_URI);
         console.log(`Connected to ${conn.connection.host}`);
-    } catch (error) {
-        console.log("Error connecting to MongoDB", error);
+    } catch (e) {
+        console.log("Error connecting to MongoDB", e);
     }
 }
