5ca7139381306c7f3b7b891cca584350449545bd
Kushagra-Dobriyal <kushagra.dobriyal@gmail.com>
2025-07-06 18:40:55 +0530

    Changes

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://chattyfy-backend:3000/api",
+    baseURL: "/api",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
