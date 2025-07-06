36e0dc6b6d4740d55ffe643baf3e58e236391171
Kushagra-Dobriyal <kushagra.dobriyal@gmail.com>
2025-07-06 18:21:13 +0530

    new commit

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ import{app,server} from './lib/socket.js'
 
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ||3000;
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));  //inc the limit of something i dont rememeber
